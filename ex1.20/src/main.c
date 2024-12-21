29a25863c9bbe4530fcb15a39fab01e8995f16ca
Amit Srivastava <xkimdr@gmail.com>
2024-12-21 09:56:51 +0530

    ex1.20 (u)

--- ex1.20/src/main.c
+++ ex1.20/src/main.c
@@ -1,47 +1,21 @@
 #include <stdio.h>
-
-void detab(char s[], int n);
+#define N 2
 
 int main(void)
 {
-    int n = 8;
-    char s[] = "\tThis is a sample text\tfor   maybe somthing.";
-
-    printf("line: %s\n", s);
-
-    detab(s, n);
-
-    printf("1 tab is replaced by %d spaces\n", n);
-
-    return 0;
-}
-
-void detab(char s[], int n)
-{
-    int i, j;
-    char r[1000];
-
-    i = j = 0;
-    while (s[i] != '\0')
+    int c;
+    while ((c = getchar()) != EOF)
     {
-        if (s[i] == '\t')
+        if (c == '\t')
         {
-            for (int k = 0; k < n; ++k)
+            for (int i = 0; i < N; ++i)
             {
-                r[j] = ' ';
-                ++j;
+                putchar(' ');
             }
-            ++i;
+            continue;
         }
-        else
-        {
-            r[j] = s[i];
-            ++i;
-            ++j;
-        }
+        putchar(c);
     }
 
-    r[j] = '\0';
-
-    printf("detab line: %s\n", r);
+    return 0;
 }
