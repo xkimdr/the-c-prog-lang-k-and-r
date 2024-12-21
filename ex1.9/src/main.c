c5ac594e15d63cefcbc30853c0f71775bb20699d
Amit Srivastava <xkimdr@gmail.com>
2024-12-21 09:35:46 +0530

    ex1.9 (c)

--- ex1.9/src/main.c
+++ ex1.9/src/main.c
@@ -1,11 +1,24 @@
 #include <stdio.h>
+#define T 1
+#define F 0
 
 int main(void)
 {
-    int c;
+    int c, b;
 
+    b = F;
     while ((c = getchar()) != EOF)
     {
+        if (c == ' ' || c == '\t')
+        {
+            if (!b)
+            {
+                putchar(' ');
+                b = T;
+            }
+            continue;
+        }
+        b = F;
         putchar(c);
     }
 
